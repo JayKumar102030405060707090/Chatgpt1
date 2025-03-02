4a10a5bc4114d2a237280108aaa2454d8f34e11a
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-02 11:02:05 +0530

    Update clonedb.py

--- ANNIEMUSIC/utils/clonedb.py
+++ ANNIEMUSIC/utils/clonedb.py
@@ -1,31 +1,29 @@
-from BADMUSIC.core.mongo import mongodb, pymongodb
-from typing import Dict, List, Union
+from ANNIEMUSIC.utils.database import mongodb
 
 cloneownerdb = mongodb.cloneownerdb
 clonebotdb = mongodb.clonebotdb
 clonebotnamedb = mongodb.clonebotnamedb
 
 
-# clone bot owner
-async def save_clonebot_owner(bot_id, user_id):
+# Save Clone Bot Owner
+async def save_clonebot_owner(bot_id: int, user_id: int):
+    """Save the owner of a cloned bot."""
     await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
 
 
-async def get_clonebot_owner(bot_id):
+async def get_clonebot_owner(bot_id: int):
+    """Retrieve the owner of a cloned bot."""
     result = await cloneownerdb.find_one({"bot_id": bot_id})
-    if result:
-        return result.get("user_id")
-    else:
-        return False
+    return result.get("user_id") if result else None
 
 
-async def save_clonebot_username(bot_id, user_name):
+# Save Clone Bot Username
+async def save_clonebot_username(bot_id: int, user_name: str):
+    """Save the username of a cloned bot."""
     await clonebotnamedb.insert_one({"bot_id": bot_id, "user_name": user_name})
 
 
-async def get_clonebot_username(bot_id):
+async def get_clonebot_username(bot_id: int):
+    """Retrieve the username of a cloned bot."""
     result = await clonebotnamedb.find_one({"bot_id": bot_id})
-    if result:
-        return result.get("user_name")
-    else:
-        return False
+    return result.get("user_name") if result else None
